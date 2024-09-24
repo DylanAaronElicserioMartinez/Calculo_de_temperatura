6f7fbc34c32077d813a1980a8796e1844e5877df
DylanAaronElicserioMartinez <a.23308051281179@cbtis128.edu.mx>
2024-09-23 20:27:55 -0600

    Add files via upload

---
+++ Calculo_de_temperatura.py
@@ -0,0 +1,7 @@
+print(" ")
+print("Dylan Aaron Elicserio Martínez esta es mi practica:")
+print(" ")
+fahrenheit = int(input("Ingresa la temperatura en fahrenheit:"))
+celsius = (fahrenheit - 32) * 5 / 9
+print(f"{fahrenheit }°F son {celsius:.2f} °C.") 
+
